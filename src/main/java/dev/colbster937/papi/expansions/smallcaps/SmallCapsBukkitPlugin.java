c9b117db010b7077168fd50e4e1bba31210cf996
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-17 14:42:48 +0500

    fix

--- src/main/java/dev/colbster937/papi/expansions/smallcaps/SmallCapsBukkitPlugin.java
+++ src/main/java/dev/colbster937/papi/expansions/smallcaps/SmallCapsBukkitPlugin.java
@@ -1,10 +1,13 @@
 package dev.colbster937.papi.expansions.smallcaps;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SmallCapsBukkitPlugin extends JavaPlugin {
-  @Override
-  public void onEnable() {
-    (new SmallCapsPAPIExpansion()).register();
-  }
+public final class Char2Caps extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("Enabled SmallCaps!");
+        new SmallCapsPAPIExpansion().register();
+    }
 }
